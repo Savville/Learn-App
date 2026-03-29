90d28d8acb28b457c64c1083e8b85833fd1b4b75
unknown <ochiwilliamotieno@gmail.com>
2026-03-29 21:30:40 +0300

    Added SSG React Snap for final boss SEO optimization

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,11 @@
+import { createRoot, hydrateRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
 
-  import { createRoot } from "react-dom/client";
-  import App from "./App.tsx";
-  import "./index.css";
+const rootElement = document.getElementById("root")!;
 
-  createRoot(document.getElementById("root")!).render(<App />);
-  
+if (rootElement.hasChildNodes()) {
+  hydrateRoot(rootElement, <App />);
+} else {
+  createRoot(rootElement).render(<App />);
+}
